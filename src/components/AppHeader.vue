231851b721cad29626baacf685d6454fb01fec51
GiadaMarzapane <giada.ortesta@gmail.com>
2023-02-02 16:51:28 +0100

    Add main + fix header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,73 +8,79 @@ export default {
 
 <template>
     <header>
-        <img src="../img/dc-logo.png" alt="">
-        <nav>
-            <ul>
-                <li>
-                    <a href="#">characters</a>
-                </li>
-                <li>
-                    <a href="#">comics</a>
-                </li>
-                <li>
-                    <a href="#">movies</a>
-                </li>
-                <li>
-                    <a href="#">tv</a>
-                </li>
-                <li>
-                    <a href="#">games</a>
-                </li>
-                <li>
-                    <a href="#">collectibles</a>
-                </li>
-                <li>
-                    <a href="#">videos</a>
-                </li>
-                <li>
-                    <a href="#">fans</a>
-                </li>
-                <li>
-                    <a href="#">news</a>
-                </li>
-                <li>
-                    <a href="#">shop</a>
-                </li>
-            </ul>
-        </nav>
+        <div class="container">
+            <img src="../img/dc-logo.png" alt="">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">characters</a>
+                    </li>
+                    <li>
+                        <a href="#">comics</a>
+                    </li>
+                    <li>
+                        <a href="#">movies</a>
+                    </li>
+                    <li>
+                        <a href="#">tv</a>
+                    </li>
+                    <li>
+                        <a href="#">games</a>
+                    </li>
+                    <li>
+                        <a href="#">collectibles</a>
+                    </li>
+                    <li>
+                        <a href="#">videos</a>
+                    </li>
+                    <li>
+                        <a href="#">fans</a>
+                    </li>
+                    <li>
+                        <a href="#">news</a>
+                    </li>
+                    <li>
+                        <a href="#">shop</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </header>
 </template>
 
 <style lang="scss" scoped>
 header{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        max-width: 1000px;
-        margin: 0 auto;
-        height: 60px;
-        & img{
+        height: 70px;
+        padding: 10px;
+        div{
+            max-width: 1000px;
+            margin: 0 auto;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
             height: 100%;
-        }
-        nav{
-            ul{
-                display: flex;
-                justify-content: center;
-                li{
-                    list-style: none;
-                    padding: 30px 15px;
-                    &:hover{
-                        color: #0282f9;
-                        border-bottom: 5px solid #0282f9;
-                    }
-                    a{
-                        text-decoration: none;
-                        color: inherit;
-                        text-transform: uppercase;
+            img{
+                height: 100%;
+            }
+            nav{
+                ul{
+                    display: flex;
+                    justify-content: center;
+                    li{
+                        list-style: none;
+                        padding: 30px 15px;
+                        &:hover{
+                            color: #0282f9;
+                            border-bottom: 5px solid #0282f9;
+                        }
+                        a{
+                            text-decoration: none;
+                            color: inherit;
+                            text-transform: uppercase;
+                        }
                     }
                 }
             }
-    }
+        }
     }
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,9 +8,25 @@ export default {
 
 <template>
     <main>
-        main
+        <div class="container">
+            <h2>
+            --> Content goes here 
+            </h2>
+        </div>
     </main>
 </template>
 
 <style lang="scss" scoped>
+main{
+        height: 15vh;
+        background-color: #1c1c1c;
+        padding: 30px;
+        div.container{
+            max-width: 1000px;
+            margin: 0 auto;
+            h2{
+                color: white;
+            }
+        }
+    }
 </style>
