b7a0dc6b50cb1d18cc9658c401ef8db45bd54153
GiadaMarzapane <giada.ortesta@gmail.com>
2023-02-02 15:29:34 +0100

    init progetto 1 parte

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+export default {
+  components: {
+    
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+<template>
+</template>
+
+<style lang="scss">
+</style>
