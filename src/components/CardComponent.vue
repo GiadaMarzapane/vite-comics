f64e16af5a28f2b7e933b10497c2b6217dc3db95
GiadaMarzapane <giada.ortesta@gmail.com>
2023-02-03 17:23:07 +0100

    Add componente per props + import nel mio appMain

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,18 @@
+<script>
+
+    export default {
+        name: 'cardComponent',
+        props:{
+            imgSrc: String,
+            imgTitle: String
+        }
+    }
+
+</script>
+
+<template>
+    <div class="card">
+        <img :src="imgSrc" :alt="imgTitle">
+        <h4>{{ imgTitle }}</h4>
+    </div>
+</template>
